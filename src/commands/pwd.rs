efe625300b5acdb4b64bbdc222bfef2b92929b97
Sayed Ahmed Husain <160347327+sahmedhusain@users.noreply.github.com>
2026-07-06 21:00:17 +0300

    Implement built-in pwd command

--- src/commands/pwd.rs
+++ src/commands/pwd.rs
@@ -2,5 +2,11 @@ use crate::types::command::ParsedCommand;
 use crate::types::errors::ShellError;
 use crate::shell::state::ShellState;
 pub fn run(_cmd: &ParsedCommand, _state: &mut ShellState) -> Result<(), ShellError> {
-    Ok(())
+    match std::env::current_dir() {
+        Ok(path) => {
+            println!("{}", path.display());
+            Ok(())
+        }
+        Err(e) => Err(ShellError::Generic(format!("pwd: {}", e))),
+    }
 }
